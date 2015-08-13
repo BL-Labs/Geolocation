7e6488a3d1e4a43d2349d72f1d23d148cc48593b
Ben O'Steen <ben.osteen@bl.uk>
2015-08-13 11:47:58 +0100

    More alterations to load order

--- GeolocationPlugin.php
+++ GeolocationPlugin.php
@@ -156,16 +156,18 @@ class GeolocationPlugin extends Omeka_Plugin_AbstractPlugin
 
     public function hookAdminHead($args)
     {
-        queue_css_file('geolocation-marker');
-        queue_css_url('//cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.3/leaflet.css');
         queue_js_url("//cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.3/leaflet.js");
+        queue_css_url('//cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.3/leaflet.css');
+        queue_css_file('geolocation-marker');
         queue_js_file('map');
     }
 
     public function hookPublicHead($args)
     {
+        queue_js_url("//cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.3/leaflet.js");
+        queue_js_url("//benosteen.com/foo.js");
+        queue_css_url('//cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.3/leaflet.css');
         queue_css_file('geolocation-marker');
-        queue_js_url("//maps.google.com/maps/api/js?sensor=false");
         queue_js_file('map');
     }
 
